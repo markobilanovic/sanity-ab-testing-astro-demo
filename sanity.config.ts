eaff482f66b3cb33a8ca0acfbb88f4bf229a9686
Marko Bilanovic <bilanovic90@gmail.com>
2026-04-06 20:46:34 +0200

    extend ssg with additional ab test slugs

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
 import { schema } from "./src/sanity/schemaTypes";
 import { abObjectCloning } from "./src/sanity/plugins/abObjectCloning";
 
 export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  plugins: [structureTool(), abObjectCloning()],
+  plugins: [structureTool(), abObjectCloning(), visionTool()],
   schema,
 });
